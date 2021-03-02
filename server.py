a57de5e0393731746564b403b617f228f44ae139
Ivan Diaz <angel.diaz@live.com.mx>
2021-03-02 17:55:47 -0600

    small change

--- server.py
+++ server.py
@@ -2,6 +2,6 @@ from gevent.pywsgi import WSGIServer
 from src.app import app
 
 if __name__ == '__main__':
-    print('Serving on 5000...')
+    print('Serving on 5000 port...')
     http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
