b0d7ce3476c1566b1ad3b4849bbf3920ec635e8d
Ivan Diaz <angel.diaz@live.com.mx>
2021-03-02 17:50:52 -0600

    uploads first files

---
+++ server.py
@@ -0,0 +1,7 @@
+from gevent.pywsgi import WSGIServer
+from src.app import app
+
+if __name__ == '__main__':
+    print('Serving on 5000...')
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
---
+++ src/app.py
@@ -0,0 +1,32 @@
+import os
+import logging
+from flask import Flask
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
+
+app.config.from_pyfile('config.py', silent=True)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/api/v1/resources/hello')
+def hello():
+    app.logger.debug('Processing default request')
+    return 'Hello, World!'
